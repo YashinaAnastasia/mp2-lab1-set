fdbda6c44ef202b14ab6e125b9f62ead4d425871
wellcome2AD <iuuyyttttt@mail.ru>
2021-09-09 13:53:52 +0300

    :((

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -21,6 +21,7 @@ TBitField::TBitField(const TBitField &bf) // конструктор копиро
 
 TBitField::~TBitField()
 {
+    delete[] pMem;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
